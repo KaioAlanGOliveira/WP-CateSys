423201003561e65fe902f84a197bd09829ac97d9
kaio <kaio@ljr.eti.br>
2026-09-17 17:14:14 -0300

    css

--- CateSysNg/src/app/shared/menu-lateral/menu-lateral.ts
+++ CateSysNg/src/app/shared/menu-lateral/menu-lateral.ts
@@ -7,4 +7,15 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
   templateUrl: './menu-lateral.html',
   styleUrl: './menu-lateral.css',
 })
-export class MenuLateral {}
+export class MenuLateral {
+  larguraMenu = '200px'; // Largura padrão (aberto)
+
+  encolher() {
+    // Se estiver em 300px, muda para 260px. Se não, volta para 300px.
+    if (this.larguraMenu === '150px') {
+      this.larguraMenu = '56px';
+    } else {
+      this.larguraMenu = '300px';
+    }
+  }
+}
